0827c0cde9e23e54778dcd4874e8f4658e60b136
Em Comeau <comeau.amanda.n@gmail.com>
2021-07-23 15:15:36 -0400

    front end skeleton AND webpack working!!!!!!!!!!

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+ 
+const path = require('path');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/client/index.tsx'],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
