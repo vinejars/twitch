6d533a0231bd1d2f759fb3002340157ae9f72e0a
Em Comeau <comeau.amanda.n@gmail.com>
2021-07-29 09:47:25 -0400

    deleted through table, friendship will be stretch goal

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -3,7 +3,6 @@ import User from "./User";
 import Post from "./Post";
 import ProfileInfo from "./ProfileInfo";
 
-User.belongsToMany(User, { as: "friends", through: "friendship" });
 User.hasMany(Post);
 Post.belongsTo(User);
 User.hasOne(ProfileInfo);
